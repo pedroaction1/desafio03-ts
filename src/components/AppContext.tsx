c143fda3263b7d4045964b4770944b0964e59f63
Pedro Henrique Andrade Borges dos Santos <pedro.santos@abainfra.com.br>
2025-12-16 17:25:40 -0300

    Alteração de erro no netlify

--- src/components/AppContext.tsx
+++ src/components/AppContext.tsx
@@ -21,7 +21,7 @@ export const AppContextProvider = ({ children }: any) => {
       }
     }, [])
 
-    const user = 'nathally'
+    const user = 'pedro@dio.bank'
   
     return (
       <AppContext.Provider value={{ user, isLoggedIn, setIsLoggedIn }}>
